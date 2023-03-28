a8952f3246b526afb15116db8b9fae5974ae45db
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-03-28 16:34:17 +0700

    completion-progress

--- components/CompletionProgress/Item.tsx
+++ components/CompletionProgress/Item.tsx
@@ -1,5 +1,5 @@
 interface ItemProps {
-    name: string,
+    title: string,
     chapter: string,
     progress: number
 }
@@ -7,9 +7,16 @@ interface ItemProps {
 const Item: React.FC<ItemProps> = (props) => {
 
     return (
-        <div className='flex flex-row w-full mx-1 bg-secondary'>
-
-        </div>
+        <li className='flex flex-row w-full h-full relative bg-red rounded-xl my-1 hover:bg-s2condCOMPLETEDBG'>
+            <div className='flex flex-col xl:w-5/6 sm:w-3/4 h-full justify-center items-center self-center'>
+                <div className="flex flex-col w-full sm:text-sm  xl:text-sm font-bold truncate absolute top-0 left-3 text-black">
+                    {props.title}
+                </div>
+                <div className="flex flex-col text-sm absolute left-3 bottom-0 mt-1">
+                    <p className="my-1 text-black">{props.chapter}</p>
+                </div>
+            </div>
+        </li>
     )
 }
 
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,9 +3,59 @@ import CompletionProgress from "@/components/CompletionProgress";
 import Layout from "@/components/Layout";
 import ProgressChart from "@/components/ProgressChart";
 import WelcomeContainer from "@/components/WelcomeContainer";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    return {
+        props: {
+            books: [
+                {
+                    title: "The Alchemist",
+                    chapter: 'Chapter 1',
+                    progress: 10,
+                },
+                {
+                    title: "Harry Potter",
+                    chapter: 'Chapter 1',
+                    progress: 20,
+                },
+                {
+                    title: "To Kill a Mockingbird",
+                    chapter: 'Chapter 1',
+                    progress: 50,
+                },
+                {
+                    title: "One Hundred Years of Solitude",
+                    chapter: 'Chapter 1',
+                    progress: 54,
+                },
+                {
+                    title: "A Passage to India",
+                    chapter: 'Chapter 1',
+                    progress: 100,
+                },
+                {
+                    title: "Invisible Man",
+                    chapter: 'Chapter 1',
+                    progress: 5,
+                },
+                {
+                    title: "Beloved",
+                    chapter: 'Chapter 1',
+                    progress: 88,
+                },
+                {
+                    title: "Things Fall Apart",
+                    chapter: 'Chapter 1',
+                    progress: 99,
+                },
+            ]
+        }
+    }
+}
 
 interface ProfileProps {
-
+    books: { title: string, chapter: string, progress: number }[]
 }
 
 const Profile: React.FC<ProfileProps> = (props) => {
@@ -16,10 +66,10 @@ const Profile: React.FC<ProfileProps> = (props) => {
                 <WelcomeContainer username='Ayo' percent={70} />
                 <div className="flex flex-row xl:w-3/5 xl:h-full sm:w-11/12 sm:h-full sm:my-5 self-center">
                     <div className="flex flex-col xl:w-full xl:h-full sm:w-4/5 mx-1 xl:mr-5 sm:mr-4">
-                        <ProgressChart/>
-                        <Calendar/>
+                        <ProgressChart />
+                        <Calendar />
                     </div>
-                    <CompletionProgress books={[["Hello", "Chapter 4", 75]]}/>
+                    <CompletionProgress books={props.books} />
                 </div>
             </div>
         </Layout>
