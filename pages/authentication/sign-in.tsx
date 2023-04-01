7adc646eb6cb003e4081690b68440403cc12d716
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-04-01 10:41:42 +0700

    update ui for sign up sign in

--- pages/authentication/sign-in.tsx
+++ pages/authentication/sign-in.tsx
@@ -29,22 +29,22 @@ const SignIn = () => {
         <div className="h-screen bg-primary p-8 text-black">
             <Logo />
             <div className="h-full w-full flex items-center justify-center">
-                <div className="shrink md:w-5/12">
+                <div className="flex flex-col justify-center shrink md:w-1/2 md:h-3/5 sm:w-4/5 sm:h-2/3 sm:p-2 sm:rounded-3xl bg-thirdLOGIN shadow-md">
                     <h2 className="text-4xl font-bold text-neutral-600 text-center mb-4">Log in to MyThorch</h2>
                     {error!==String(null) && <p className='text-red-600'>{error}</p>}
-                    <label className="font-semibold">Email Address</label>
+                    <label className="font-semibold md:text-xl">Email Address</label>
                     <input type="email" name="email" placeholder="Email Address" 
                         onChange={(e) => setEmail(e.target.value)}
                         className="w-full my-4 px-4 py-2 bg-tertiary rounded-full text-black" />
-                    <label className="font-semibold text-black my-1">Password</label>
+                    <label className="font-semibold text-black my-1 md:text-xl">Password</label>
                     <input type="password" name="password" placeholder="Password"
                         onChange={(e) => setPassword(e.target.value)}
                         className="w-full my-4 px-4 py-2 bg-tertiary rounded-full text-black" />
-                    <Link href="#" className="text-secondary font-bold">I forgot my password</Link>
+                    <Link href="#" className="text-secondary font-bold text-sm">Forget password?</Link>
                     <button
                         onClick={submitHandler}
-                        className="bg-secondary rounded-full w-full my-4 p-2 text-neutral-200 font-bold hover:scale-x-105 transition">Login</button>
-                    <p className="text-secondary mt-5">Don&apos;t have an account?&nbsp;
+                        className="bg-secondary rounded-full w-full my-4 p-2 text-neutral-200 font-bold hover:bg-white hover:text-secondary">Login</button>
+                    <p className="text-secondary mt-5 text-sm">Don&apos;t have an account?&nbsp;
                         <Link href="/authentication/sign-up" className="hover:underline">Register</Link>.
                     </p>
                 </div>
--- pages/authentication/sign-up.tsx
+++ pages/authentication/sign-up.tsx
@@ -23,25 +23,25 @@ const SignUp = () =>
         <div className="h-screen bg-primary p-8 text-black">
             <Logo />
             <div className="h-full w-full flex items-center justify-center">
-                <div className="shrink md:w-5/12">
+                <div className="flex flex-col p-3 sm:rounded-3xl shrink md:w-1/2 md:h-3/5 justify-center bg-thirdLOGIN shadow-md">
                     <h2 className="text-4xl font-bold text-neutral-600 text-center mb-4">Get started for free</h2>
                     {error!==String(null) && <p className='text-red-600'>{error}</p>}
-                    <label className="font-semibold">Email Address</label>
+                    <label className="font-semibold md:text-md">Email Address</label>
                     <input type="email" name="email" placeholder="Email Address" 
                         onChange={(e) => setEmail(e.target.value)}
                         className="w-full my-4 px-4 py-2 bg-tertiary rounded-full text-black" />
-                    <label className="font-semibold text-black">Password</label>
+                    <label className="font-semibold text-black md:text-md">Password</label>
                     <input type="password" name="password" placeholder="Password"
                         onChange={(e) => setPassword(e.target.value)}
                         className="w-full my-4 px-4 py-2 bg-tertiary rounded-full text-black" />
                     <button
                         onClick={submitHandler}
-                        className="bg-secondary rounded-full w-full mb-4 p-2 text-neutral-200 font-bold hover:scale-x-105 transition">Sign up</button>
-                    <p className="text-secondary mb-4">By signing up for MyThorch, you agree to MyThorch&apos;s&nbsp;
+                        className="bg-secondary rounded-full w-full mb-4 p-2 text-neutral-200 font-bold hover:bg-white hover:text-secondary">Sign up</button>
+                    <p className="text-secondary mb-4 text-sm">By signing up for MyThorch, you agree to MyThorch&apos;s&nbsp;
                         <a href="#" className="hover:underline">Terms of Service</a> &&nbsp;
                         <a href="#" className="hover:underline">Privacy Policy</a>.
                     </p>
-                    <p className="text-secondary">Already have an account?&nbsp;
+                    <p className="text-secondary text-sm">Already have an account?&nbsp;
                         <Link href="/authentication/sign-in" className="hover:underline">Log in</Link>.
                     </p>
                 </div>
